247736e197ded58669260a8f9d5a26508517004f
gaganashreer22-hue <gaganashreer.22@gmail.com>
2025-12-29 15:17:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 import streamlit as st
 
-st.title("AI Career Guidance System")
+st.set_page_config(page_title="Login Page", page_icon="🔐")
 
-st.write("This is my career guidance website.")
+# Create login state
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+st.title("🔐 Login Page")
+
+if st.session_state.logged_in == False:
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if username == "student" and password == "1234":
+            st.session_state.logged_in = True
+            st.success("Login successful!")
+        else:
+            st.error("Wrong username or password")
+
+else:
+    st.title("🎓 Welcome")
+    st.write("Login successful. You are inside the website.")
